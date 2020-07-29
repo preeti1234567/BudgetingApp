6cf140b0d5eb07fb614162d9c62273b8a61ec633
Preeti Gupta <preetigupta510@gmail.com>
2020-07-29 14:10:24 -0700

    some correction in api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,43 +1,3 @@
-var db = require("../models")
-
-module.exports = function(app) {
-    //Server routes go here
-}
-
-module.exports = function(app) {
-    app.get("/api/authors", function(req, res) {
-      db.Author.findAll({}).then(function(dbAuthor) {
-        res.json(dbAuthor);
-      });
-    });
-  
-    app.get("/api/authors/:id", function(req, res) {
-      db.Author.findOne({
-        where: {
-          id: req.params.id
-        }
-      }).then(function(dbAuthor) {
-        res.json(dbAuthor);
-      });
-    });
-  
-    app.post("/api/authors", function(req, res) {
-      db.Author.create(req.body).then(function(dbAuthor) {
-        res.json(dbAuthor);
-      });
-    });
-  
-    app.delete("/api/authors/:id", function(req, res) {
-      db.Author.destroy({
-        where: {
-          id: req.params.id
-        }
-      }).then(function(dbAuthor) {
-        res.json(dbAuthor);
-      });
-    });
-
-    
        //Get route for the income
        app.get("/api/income", function(req, res) {
         db.Income.findAll({}).then(function(data){
@@ -207,5 +167,5 @@ module.exports = function(app) {
       });
 
   
-  };
+
   
