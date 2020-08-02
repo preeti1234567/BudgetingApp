e2167bc3338ef24a007fdfbc60b4e86f0be0b629
Preeti Gupta <preetigupta510@gmail.com>
2020-08-02 07:28:02 -0700

    work in models

--- models/oneTimePurchase.js
+++ models/oneTimePurchase.js
@@ -2,7 +2,7 @@ module.exports = function (sequelize, DataTypes) {
     var oneTimePurchase = sequelize.define("oneTimePurchase", {
         title: DataTypes.STRING,
         amount: DataTypes.FLOAT,
-        date: DataTypes.STRING
+       date: DataTypes.STRING
     })
     oneTimePurchase.associate = function (models) {
         oneTimePurchase.belongsTo(models.User, { foreignKey: {allowNull:false}})
